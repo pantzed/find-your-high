c935061f9733daf33b176fdab34098c3f9d6c8ae
Edward <edpantzar@gmail.com>
2018-05-24 14:39:18 -0500

    Cleaned up app.js

--- app.js
+++ app.js
@@ -1,70 +1,64 @@
 (function() {
-let strains = [];
-let globalSearchString;
+  let strains = [];
+  let globalSearchString;
 
-document.querySelector('#search-results-list').addEventListener("click", updateStrainInfo);
+  document.getElementById('search-form').addEventListener('submit', processSubmit);
+  document.getElementById('search-results-list').addEventListener("click", updateStrainInfo);
 
-function updateStrainInfo(){
-  let strainIndex = event.target.getAttribute('strain_index');
-  document.getElementById('display-strain-name').textContent = strains[0][strainIndex].name;
-  if (strains[0][strainIndex].desc === null){
-    document.getElementById('strain-description').textContent = "No description Available!";
-  }
-  else {
-    document.getElementById('strain-description').textContent = strains[0][strainIndex].desc;
-  }
-}
-
-function displayAllReturnedStrains() {
-  document.getElementById('search-results-list').innerHTML = "";
-  strains[0].forEach(function(strain){
-    let strainLink = document.createElement('a');
-    strainLink.setAttribute('strain_index', strains[0].indexOf(strain));
-    strainLink.setAttribute('href', "#info-row");
-    strainLink.innerHTML = strain.name;
-    let strainName = document.createElement('li');
-    strainName.appendChild(strainLink);
-    document.getElementById('search-results-list').appendChild(strainName);
-  });
-}
-
-function noExactMatchAlert() {
-  document.querySelector('#alert > p').innerHTML = (`* No exact result for "${globalSearchString}"`).toUpperCase();
-}
-
-function renderFirstStrainInfo() {
-  console.log(strains);
-  displayAllReturnedStrains();
-  let strainIndex = 0;
-  let exact = false;
-  strains[0].forEach(function(strain){
-    if (strain.name.toLowerCase() === globalSearchString.toLowerCase()){
-      strainIndex = strains[0].indexOf(strain);
-      exact = true;
+  function updateStrainInfo(){
+    let strainIndex = event.target.getAttribute('strain_index');
+    document.getElementById('display-strain-name').textContent = strains[0][strainIndex].name;
+    if (strains[0][strainIndex].desc === null){
+      document.getElementById('strain-description').textContent = "No description Available!";
     }
-  });
-
-  if (exact === false){
-    noExactMatchAlert();
-  }
-  else {
-    document.querySelector('#alert > p').innerHTML = "";
+    else {
+      document.getElementById('strain-description').textContent = strains[0][strainIndex].desc;
+    }
   }
 
-  document.getElementById('display-strain-name').textContent = strains[0][strainIndex].name;
-
-  if (strains[0][0].desc === null){
-    document.getElementById('strain-description').textContent = "No description Available!";
+  function displayAllReturnedStrains() {
+    document.getElementById('search-results-list').innerHTML = "";
+    strains[0].forEach(function(strain){
+      let strainLink = document.createElement('a');
+      strainLink.setAttribute('strain_index', strains[0].indexOf(strain));
+      strainLink.setAttribute('href', "#info-row");
+      strainLink.innerHTML = strain.name;
+      let strainName = document.createElement('li');
+      strainName.appendChild(strainLink);
+      document.getElementById('search-results-list').appendChild(strainName);
+    });
   }
-  else {
-    document.getElementById('strain-description').textContent = strains[0][strainIndex].desc;
+
+  function noExactMatchAlert() {
+    document.querySelector('#alert > p').innerHTML = (`* No exact result for "${globalSearchString}"`).toUpperCase();
   }
 
-  document.getElementById('strain-race').textContent = `Type: ${strains[0][strainIndex].race}`;
-
-}
-
-document.getElementById('search-form').addEventListener('submit', processSubmit);
+  function renderFirstStrainInfo() {
+    console.log(strains);
+    displayAllReturnedStrains();
+    let strainIndex = 0;
+    let exact = false;
+    strains[0].forEach(function(strain){
+      if (strain.name.toLowerCase() === globalSearchString.toLowerCase()){
+        strainIndex = strains[0].indexOf(strain);
+        exact = true;
+      }
+    });
+    if (exact === false){
+      noExactMatchAlert();
+    }
+    else {
+      document.querySelector('#alert > p').innerHTML = "";
+    }
+    document.getElementById('display-strain-name').textContent = strains[0][strainIndex].name;
+    if (strains[0][0].desc === null){
+      document.getElementById('strain-description').textContent = "No description Available!";
+    }
+    else {
+      document.getElementById('strain-description').textContent = strains[0][strainIndex].desc;
+    }
+    document.getElementById('strain-race').textContent = `Type: ${strains[0][strainIndex].race}`;
+  }
 
   function processSubmit() {
     strains = [];
@@ -104,4 +98,5 @@ document.getElementById('search-form').addEventListener('submit', processSubmit)
       renderFirstStrainInfo();
     });
   }
+
 })();
