9c608fabe0424233a47bbc231922dbe3092ec4c5
Edward <edpantzar@gmail.com>
2018-05-24 15:30:04 -0500

    Fixed footer positioning

--- page-mods.js
+++ page-mods.js
@@ -1,6 +1,21 @@
 (function() {
-    let mainContainer = document.getElementById('main-container');
-    
-    mainContainer.style.height = `${window.innerHeight}px`;
-    console.log(mainContainer);
+
+    // window.addEventListener('resize', positionFooter);
+    document.getElementById('main-container').addEventListener("change", rePositionFooter);
+
+    function positionFooter() {
+        console.log(true);
+        let mainContainer = document.getElementById('main-container');
+        let height = Math.max(document.documentElement.clientHeight);
+        mainContainer.style.height = `${height}px`;
+    }
+
+    function rePositionFooter() {
+        console.log(false);
+        let mainContainer = document.getElementById('main-container');
+        mainContainer.style.height = `100%`;
+    }
+
+    positionFooter();
+
 })();
